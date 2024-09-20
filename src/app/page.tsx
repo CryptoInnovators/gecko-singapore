b64a062af699090f03fd14c1638e14776ccda185
nkoorty <amalyshau2002@gmail.com>
2024-09-21 00:09:34 +0800

    added auth and subscription page

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className="mb-12 mt-28 sm:mt-4an flex flex-col items-center justify-center text-center">
+
+        <p className="txt-sm font-semibold text-gray-700">
+          Gecko is now Public!
+        </p>
+
+        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+          Find <span className="text-red-600">Vulnerabilities</span> in your
+          Clarity Contracts
+        </h1>
+        <p className="mt-5 max-w-prose text-white sm:text-lg">
+          Gecko automatically finds security vulnerabilities in your contracts.
+          Simply upload your contract and Gecko will find and remediate bugs.
+        </p>
+      </div>
+    </>
+  );
+}
---
+++ src/app/subscription/page.tsx
@@ -0,0 +1,11 @@
+import Price from '@/components/subscription/Pricing'
+import React from 'react'
+//import Price from '../../components/subscription/price'
+
+export default function page() {
+  return (
+    <div className="mx-auto max-w-7xl px-4">
+          <Price/>
+    </div>
+  )
+}
